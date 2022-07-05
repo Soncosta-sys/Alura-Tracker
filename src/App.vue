af6a6362e778c1f50fbfa4ecc392b7f0085c2bbd
talison <talisonmedeiros92@gmail.com>
2022-07-05 11:02:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="columns is-fapless is-multiline">
+    <div class="columns is-one-quarter">
+
+    </div>
+    <div class="columns is-one-quarter"></div>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App'
+
+});
+</script>
+
+<style>
+</style>
